8e0efad311aa638dea650ed6f80ab00943ed9949
Danny-Kyle <dikedaniel7917@gmail.com>
2023-11-13 08:31:54 +0100

    final

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,29 +7,49 @@ import insta from "../../assets/instagram.png"
 import stackoverflow from "../../assets/stackoverflow_icon-icons.com_62748.ico" 
 import emailjs from '@emailjs/browser';
 import { useRef } from "react"
+import { useState } from "react"
 
 const Contact = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
     const form = useRef();
     const sendEmail = (e) => {
         e.preventDefault();
+
+        const serviceId = 'service_3tzvu26';
+        const templateId = 'template_wctjivs';
+        const publicKey  ='XqrYkLElz0Hu2BszI';
+        const templateParams = {
+            from_name: name,
+            from_email: email,
+            to_name: 'Daniel',
+            message: message
+          }
     
-        emailjs.sendForm('service_3tzvu26', 'template_wctjivs', form.current, 'XqrYkLElz0Hu2BszI')
+        emailjs.send(serviceId, templateId, form.current, templateParams, publicKey)
           .then((result) => {
               console.log(result.text);
-              e.target.reset()
               alert('Email Sent')
-          }, (error) => {
-              console.log(error.text);
-          });
+              setName('')
+              setMessage('')
+              setEmail('')
+          })
+          .catch((error) => {
+            console.error('Error sending mail', error);
+        });
+          
       };
+
+      
     return (
         <section id="contactPage">
             <h1 className="contactPageTitle">Contact Me</h1>
             <span className="contactDesc">Please fill out the form below to discuss any collaboration ideas</span>
             <form className="contactForm" ref={form} onSubmit={sendEmail}>
-                <input type="text" className="name" placeholder="Your Name" name="your_name"/>
-                <input type="text" className="email" placeholder="Your Email Address" name="your_email" />
-                <textarea className="msg" name="message" rows="5" placeholder="Message....."></textarea>
+                <input type="text" className="name" placeholder="Your Name" value={name} onChange={(e)=> setName(e.target.value)}/>
+                <input type="text" className="email" placeholder="Your Email Address" value={email} onChange={(e)=> setEmail(e.target.value)}/>
+                <textarea className="msg" value={message} rows="5" placeholder="Message....." onChange={(e)=> setMessage(e.target.value)}></textarea>
                 <button type="submit" value='Send' className="submitBtn">Submit</button>
                 <div className="links">
                    <a href="https://github.com/Danny-Kyle/"><img src={github} alt="github" className="link" /></a> 
